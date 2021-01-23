5bfb9d28cc9cadff8b583944a9704dc14c4fee7e
Jimyang0724 <jimyang890724@gmail.com>
2021-01-23 17:00:05 +0800

    Can choose "Bad"

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.tiptime
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import androidx.appcompat.app.AlertDialog
 import com.example.tiptime.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import java.text.NumberFormat
 import java.util.*
 import kotlin.math.ceil
@@ -16,8 +21,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener {
-            calculateTip()
+            if(binding.costOfService.text.isEmpty()) showDialog()
+            else calculateTip()
         }
+    }
+
+    fun showDialog(){
+        val builder = AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage("Input cost of service!")
+                .setPositiveButton("OK", null)
+                .show()
     }
 
     fun calculateTip(){
@@ -30,8 +44,10 @@ class MainActivity : AppCompatActivity() {
         val tipPercentage = when(selectID) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
+            R.id.option_fifteen_percent -> 0.15
+            else -> 0.0
         }
+        if(selectID == R.id.option_bad) linkToWeb("https://www.youtube.com/watch?v=_6TtTRrno3E")
 
         // calculate
         var tip = cost*tipPercentage
@@ -39,5 +55,11 @@ class MainActivity : AppCompatActivity() {
         if(roundUp) tip = ceil(tip)
         val formattedTip = NumberFormat.getCurrencyInstance(Locale.TAIWAN).format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    fun linkToWeb(url: String){
+        val website = Intent(Intent.ACTION_VIEW)
+        website.data = Uri.parse(url)
+        startActivity(website)
     }
 }
